7a41ad3c282046f61da1b8907e1ee716edf03345
eczn <1975845422@qq.com>
2018-04-13 00:00:23 +0800

    [+] dest new [file_name]

--- src/cli/dest-new.js
+++ src/cli/dest-new.js
@@ -1,25 +1,53 @@
 #!/usr/bin/env node
+const { 
+        pkg_conf, resolve, tpl 
+    } = require('./cli-utils')
+    , child_process = require('child_process')
+    , program = require('commander')
+    , path = require('path')
+    , fs = require('fs')
+    
 
-const { pkg_conf, BASE, resolve } = require('./cli-utils')
+/**
+ * @description 创建 daily 
+ * @param { String } file_name 
+ */
+async function newDaily(file_name){
+    let love_base = resolve('store/love'); 
 
-const program = require('commander');
+    let daily_init = tpl.daily({
+        title: file_name, 
+        time: (new Date()).toString(), 
+        from: 'eczn', 
+        to: 'deswan', 
+        theme: 'pure'
+    }); 
 
-let todo_table = {
-    daily(title){
+    // Write File 
+    let daily_path = path.join(love_base, file_name + '.md'); 
 
-    }, 
-    img(){
-
+    if (fs.existsSync(daily_path)){
+        console.log(`  Error: 文件已存在, 换个名字把;`); 
+        console.log(`  ${daily_path}`); 
+        return; 
     }
+
+    fs.writeFileSync(daily_path, daily_init, 'utf-8'); 
+
+    // Log 
+    console.log('File Generated:'); 
+    console.log('  ' + daily_path)
+    console.log('  '); 
+
+    // Start VSCode 
+    console.log('Starting Editor ... '); 
+    child_process.exec(`code ${ resolve() } ${ daily_path }`); 
 }
 
 program
     .version(pkg_conf.version)
-    .arguments('<target_obj> [title]')
-    .action((target, title) => {
-        console.log(target); 
-        console.log(title); 
-    })
+    .arguments('[file_name]')
+    .action(newDaily)
     .parse(process.argv);
 // 
 

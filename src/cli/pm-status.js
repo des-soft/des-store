8a81bf5163cb06d2705f918bfef373224d944656
eczn <1975845422@qq.com>
2018-04-12 11:08:31 +0800

    [+] count for mail-img

--- src/cli/pm-status.js
+++ src/cli/pm-status.js
@@ -11,6 +11,7 @@ var resolve = $ => path.join(config.git_base, $);
 
 // All 
 let love_base = resolve('store/love'); 
+let mail_img_base = resolve('store/mail-img'); 
 
 // Loading  
 let loading = fs.readdirSync(love_base).map(file_name => {
@@ -22,15 +23,20 @@ let loading = fs.readdirSync(love_base).map(file_name => {
             path: file
         }
     })
-})
+}); 
+
+let imgs = fs.readdirSync(mail_img_base); 
 
 Promise.all(loading).then(files => {
     let length = files.reduce((acc, cur) => {
         return acc + cur.text.length
     }, 0); 
 
-    console.log('  Count: store/love/*.md')
-    console.log('    共计 %d 篇', files.length)
+    console.log('  Count: store/love/*');
+    console.log('    共计 %d 篇', files.length);
     console.log('    总长度 %d', length); 
+    console.log('    '); 
+    console.log('  Count: store/mail-img/*');
+    console.log('    共计 %d 张', imgs.length);
 }); 
 
